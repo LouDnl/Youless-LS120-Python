5ffc821c1c106b015a08b5316fee0f1675b1bd2a
LouD <thierry@smeekes.nl>
2022-11-22 10:56:16 +0100

    Update dash_settings.py

--- dash_settings.py
+++ dash_settings.py
@@ -14,6 +14,7 @@ import logging
 logger = logging.getLogger(__name__)
 logger.debug("dash_settings.py loaded")
 
+# TODO - random todo for testing
 
 class Dash_Settings:
     """dash default debug, ip and port settings"""
